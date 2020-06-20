d46c685af9df111587a0e17d1d9e243eebd37baf
Tweea <tweea@263.net>
2020-06-20 16:32:05 +0800

    规则排序工具

--- 开发标准/编码规范/Sonar/SonarProfileSorter.java
+++ 开发标准/编码规范/Sonar/SonarProfileSorter.java
@@ -1,33 +1,68 @@
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.CompareToBuilder;
 
-public class EclipseSettingSorter {
-	public static void main(String[] args) throws IOException {
+public class SonarProfileSorter {
+	public static void main(String[] args)
+		throws IOException {
 		File inputFile = new File(args[0]);
 		File outputFile = new File(args[1]);
 
 		List<String> inputLines = FileUtils.readLines(inputFile, "UTF-8");
 		List<String> startLines = new ArrayList<>();
 		List<String> endLines = new ArrayList<>();
-		List<String> settingLines = new ArrayList<>();
+		List<Rule> rules = new ArrayList<>();
+		Rule currentRule = null;
 		for (String line : inputLines) {
-			if (line.startsWith("<setting")) {
-				settingLines.add(line);
-			} else if (settingLines.isEmpty()) {
-				startLines.add(line);
+			String trimedLine = line.trim();
+			if (trimedLine.equals("<rule>")) {
+				currentRule = new Rule();
+				rules.add(currentRule);
+				currentRule.lines.add(line);
+			} else if (trimedLine.equals("</rule>")) {
+				currentRule.lines.add(line);
+				currentRule = null;
+			} else if (currentRule == null) {
+				if (rules.isEmpty()) {
+					startLines.add(line);
+				} else {
+					endLines.add(line);
+				}
 			} else {
-				endLines.add(line);
+				if (currentRule.repositoryKey == null && trimedLine.startsWith("<repositoryKey>")) {
+					currentRule.repositoryKey = StringUtils.substringBetween(trimedLine, "<repositoryKey>", "</repositoryKey>");
+				} else if (currentRule.key == null && trimedLine.startsWith("<key>")) {
+					currentRule.key = StringUtils.substringBetween(trimedLine, "<key>", "</key>");
+				}
+				currentRule.lines.add(line);
 			}
 		}
-		Collections.sort(settingLines);
+		Collections.sort(rules, (r1, r2) -> new CompareToBuilder().append(r1.repositoryKey, r2.repositoryKey).append(r1.key, r2.key).toComparison());
 
-		FileUtils.writeLines(outputFile, "UTF-8", startLines, "\n");
-		FileUtils.writeLines(outputFile, "UTF-8", settingLines, "\n", true);
-		FileUtils.writeLines(outputFile, "UTF-8", endLines, "\n", true);
+		try (OutputStream os = new FileOutputStream(outputFile)) {
+			IOUtils.writeLines(startLines, "\n", os, StandardCharsets.UTF_8);
+			for (Rule rule : rules) {
+				IOUtils.writeLines(rule.lines, "\n", os, StandardCharsets.UTF_8);
+			}
+			IOUtils.writeLines(endLines, "\n", os, StandardCharsets.UTF_8);
+		}
+	}
+
+	private static class Rule {
+		String repositoryKey;
+
+		String key;
+
+		List<String> lines = new ArrayList();
 	}
 }
