d26c54e7a742512e94c6fc94e13e7df8e6f92429
Tweea <tweea@263.net>
2020-06-21 19:30:57 +0800

    更新 eclipse 包工具

--- 开发工具/eclipse/EclipsePackageMaker.java
+++ 开发工具/eclipse/EclipsePackageMaker.java
@@ -1,18 +1,22 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class EclipsePackageMaker {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args)
+		throws IOException {
 		File sourceDir = new File(args[0]);
 		File targetDir = new File(args[1]);
 		File outputFile = new File(args[2]);
@@ -21,14 +25,16 @@ public class EclipsePackageMaker {
 		List<String> removedList = new ArrayList<>();
 		visitDir(sourceDir, targetDir, null, changedList, removedList);
 
-		FileUtils.writeLines(outputFile, "UTF-8", Arrays.asList("Changed Files:"), "\n");
-		FileUtils.writeLines(outputFile, "UTF-8", changedList, "\n", true);
-		FileUtils.writeLines(outputFile, "UTF-8", Arrays.asList("", "Removed Files:"), "\n", true);
-		FileUtils.writeLines(outputFile, "UTF-8", removedList, "\n", true);
+		try (OutputStream os = new FileOutputStream(outputFile)) {
+			IOUtils.writeLines(Arrays.asList("Changed Files:"), "\n", os, StandardCharsets.UTF_8);
+			IOUtils.writeLines(changedList, "\n", os, StandardCharsets.UTF_8);
+			IOUtils.writeLines(Arrays.asList("", "Removed Files:"), "\n", os, StandardCharsets.UTF_8);
+			IOUtils.writeLines(removedList, "\n", os, StandardCharsets.UTF_8);
+		}
 	}
 
-	private static void visitDir(File sourceDir, File targetDir, String basePath, List<String> changedList,
-			List<String> removedList) throws IOException {
+	private static void visitDir(File sourceDir, File targetDir, String basePath, List<String> changedList, List<String> removedList)
+		throws IOException {
 		if (!targetDir.exists()) {
 			removedList.add(basePath);
 			return;
@@ -57,8 +63,8 @@ public class EclipsePackageMaker {
 		}
 	}
 
-	private static void visitFile(File sourceFile, File targetFile, String basePath, List<String> changedList,
-			List<String> removedList) throws IOException {
+	private static void visitFile(File sourceFile, File targetFile, String basePath, List<String> changedList, List<String> removedList)
+		throws IOException {
 		if (!targetFile.exists()) {
 			removedList.add(basePath);
 			return;
@@ -85,7 +91,8 @@ public class EclipsePackageMaker {
 		}
 	}
 
-	private static String hashFileContent(File file) throws IOException {
+	private static String hashFileContent(File file)
+		throws IOException {
 		try (InputStream is = new FileInputStream(file)) {
 			return DigestUtils.sha512Hex(is);
 		}
